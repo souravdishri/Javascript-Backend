ec7e1fdd8a666cadfe59779c582d3a715c1b62b2
Sourav Dishri <souravdishri@gmail.com>
2024-06-10 12:11:08 +0530

    config cors & cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 import express from "express";
+import cors from "cors"
+//Perform CRUD operation in user's browser
+import cookieParser from "cookie-parser"
 
 const app = express();
+
+//To use middleware/configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
 
 export { app }
