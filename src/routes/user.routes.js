93d399b866899487ae499303900604a078dd4c75
Sourav Dishri <souravdishri@gmail.com>
2025-08-08 02:43:50 +0530

    fix: correct typo in comment for import statement

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-//we can import like this when it is not export in defa
+//we can import like this when it is not export in default
 import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router()
