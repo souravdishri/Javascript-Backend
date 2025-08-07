46cafd9567b950dddba251aa40369f036f6c1b49
Sourav Dishri <souravdishri@gmail.com>
2025-08-08 02:44:04 +0530

    fix: update import path for asyncHandler middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { asyncHandler } from "../middlewares/asyncHandler.js";
 
 
 const registerUser = asyncHandler( async (req, res) => {
