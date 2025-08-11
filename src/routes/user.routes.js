838a4d5899d5f419d5680cd20c03af570f596bfd
Sourav Dishri <souravdishri@gmail.com>
2025-08-11 12:51:20 +0530

    refactor: add comments for clarity and understanding of user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,3 +1,5 @@
+// src/routes/user.routes.js
+
 import { Router } from "express";
 //we can import like this when it is not export in default
 import { registerUser } from "../controllers/user.controller.js";
@@ -9,3 +11,16 @@ router.route("/register").post(registerUser)
 
 
 export default router
+
+
+
+// This `router` is a `mini Express app` that handles routes like `/register`. 
+// When you export default router, you're saying:
+// “Here’s a bundle of routes—ready to be plugged into the app.js file.” 
+
+// here we are exporting this router instance as the default export from this file and importing it in `app.js`
+// (importing the default export from `user.routes.js` and calling it `userRouter`)
+
+// Because `we can name default imports anything we want`.
+// Even though we exported it as router, we are not importing a variable called `router`, 
+// we are importing the default export, and giving it a name locally `(userRouter)` to make your our readable and meaningful.
