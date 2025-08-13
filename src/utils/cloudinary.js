fa2256d69b4094046b734178bfccaac8b12caf42
Sourav Dishri <souravdishri@gmail.com>
2025-08-13 18:56:12 +0530

    fix typo in comment for desc

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -14,7 +14,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        // file has been uploaded successfull
+        // file has been uploaded successfully
         //console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
         return response;
