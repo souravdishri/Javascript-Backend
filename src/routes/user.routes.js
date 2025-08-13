0887dfd900f69981cd9ff1014e9e54c378d1db58
Sourav Dishri <souravdishri@gmail.com>
2025-08-13 16:24:57 +0530

    refactor: enhance file upload handling in user registration route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,11 +3,36 @@
 import { Router } from "express";
 //we can import like this when it is not export in default
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    // mostly middlewares are used to add more fields to the request object
+
+    // `upload` is a middleware that handles file uploads using multer
+    // `upload.fields` is used to handle multiple file uploads with different field names
+    // `avatar` and `coverImage` are the field names in the form
+    // `maxCount` is used to limit the number of files that can be uploaded for each field
+    
+    // `req.files` will contain the uploaded files, where `avatar` and `coverImage` are arrays of files
+    // `req.files.avatar[0].path` is used to get the path of the uploaded avatar file
+    // `req.files.coverImage[0].path` is used to get the path of the uploaded cover image file
+    
+    // `uploadOnCloudinary` is a function that uploads the file to Cloudinary and returns the response
+    upload.fields([
+        {
+            name: "avatar", 
+            maxCount: 1 
+        },
+        { 
+            name: "coverImage", 
+            maxCount: 1 
+        }
+    ]),
+    registerUser
+)
 
 
 export default router
