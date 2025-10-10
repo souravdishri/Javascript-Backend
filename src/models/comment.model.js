fa692662cb0ba6ef9b1cc1311216a9d358e7c50d
Sourav Dishri <souravdishri@gmail.com>
2025-10-10 16:42:03 +0530

    add comment for desc

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -24,4 +24,21 @@ const commentSchema = new Schema(
 
 commentSchema.plugin(mongooseAggregatePaginate)
 
+// this plugin automatically:
+// Adds `$skip` and `$limit` under the hood based on your pagination `options`.
+// Returns `total count`, `total pages`, `current page`, `hasNextPage`, etc.
+// Prevents you from manually messing up the skip/limit math.
+
+
 export const Comment = mongoose.model("Comment", commentSchema)
+
+
+
+// note: When to use `$skip`/`$limit` vs `mongoose-aggregate-paginate-v2`
+
+// | Situation                                                             | Use `$skip` / `$limit` | Use `aggregatePaginate` |
+// | --------------------------------------------------------------------- | ---------------------- | ----------------------- |
+// | You want full manual control                                          | ✅ Yes                  | ❌ No                    |
+// | You’re already using `mongoose-aggregate-paginate-v2`                 | ❌ No                   | ✅ Yes                   |
+// | You just need simple pagination in one place                          | ✅ Yes                  | ❌ No                    |
+// | You’ll reuse pagination logic in many places (comments, videos, etc.) | ❌ No                   | ✅ Yes                   |
